88741ad7b2ac60a970b758015ab06251d000b316
Khushboo Tiwari <ktiwari1991@gmail.com>
2019-04-08 21:28:29 +0530

    Update views.py

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,27 +1,13 @@
 from django.shortcuts import render
-import datetime
+from blog.models import Post
+from django.contrib.auth.models import User
+
 #from django.http import HttpResponse
-
-posts =[
-    {
-        'author' : 'Corey MS',
-        'title' : 'Blog Post 1',
-        'date_posted' : datetime.date.today(),
-        'content' : 'First Post content'
-    },
-     {
-        'author' : 'Jane MS',
-        'title' : 'Blog Post 2',
-        'date_posted' : datetime.date.today(),
-        'content' : 'Second Post content'
-    },
-]
-
 
 def home(request):
     #return HttpResponse("<h1>Blog Home </h1>")
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request,"blog/home.html",context)
 
